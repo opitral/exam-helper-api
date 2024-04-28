5dac9a6777661d5b8023ceed9077e199f4467b1d
Володимир Слободян <opitral@proton.me>
2024-04-28 23:09:07 +0300

    change text messages

--- app.py
+++ app.py
@@ -15,58 +15,41 @@ helper = getenv("TELEGRAM_HELPER_ID")
 @app.get("/up")
 async def move_up():
     print("up")
-    bot.send_message(helper, "up")
+    bot.send_message(helper, "Нахил вверх")
     return {"message": "Moving up!"}
 
 
 @app.get("/down")
 async def move_down():
     print("down")
-    bot.send_message(helper, "down")
+    bot.send_message(helper, "Нахил вниз")
     return {"message": "Moving down!"}
 
 
 @app.get("/right")
 async def move_right():
     print("right")
-    bot.send_message(helper, "right")
+    bot.send_message(helper, "Нахил вправо")
     return {"message": "Moving right!"}
 
 
 @app.get("/left")
 async def move_left():
     print("left")
-    bot.send_message(helper, "left")
+    bot.send_message(helper, "Нахил вліво")
     return {"message": "Moving left!"}
 
 
 @app.get("/tap")
 async def move_left():
     print("tap")
-    bot.send_message(helper, "tap")
+    bot.send_message(helper, "Тап по екрану")
     return {"message": "Tap!"}
 
 
 @app.get("/deploy")
 async def deploy():
-    # os.system('''
-    #     cd ~/exam-helper-bot;
-    #     git pull;
-    #     pm2 restart exam;
-    #     cd ~/exam-helper-api;
-    #     git pull;
-    #     pm2 restart 1488;
-    # ''')
-
-    # subprocess.run(['sh ~/exam-hepler-api/deploy.sh'])
-    # subprocess.run(['git', 'pull'])
-    # subprocess.run(['pm2', 'restart exam'])
-    # subprocess.run(['cd', '~/exam-helper-api'])
-    # subprocess.run(['git', 'pull'])
-    # subprocess.run(['pm2', 'restart 1488'])
-    process = subprocess.run('sh deploy.sh', shell=True) 
-
-
+    subprocess.run('sh deploy.sh', shell=True)
     return {"message": "Deployed!"}
 
 
