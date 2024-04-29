80240a402c46eedbeaf380399ddd68bae0e9fab2
Володимир Слободян <opitral@proton.me>
2024-04-29 03:09:00 +0300

    change text messages

--- app.py
+++ app.py
@@ -14,38 +14,38 @@ helper = getenv("TELEGRAM_HELPER_ID")
 
 @app.get("/up")
 async def move_up():
-    bot.send_message(helper, "Нахил вверх")
-    return {"message": "Moving up!"}
+    bot.send_message(helper, "Наступне питання")
+    return {"message": ""}
 
 
 @app.get("/down")
 async def move_down():
-    bot.send_message(helper, "Нахил вниз")
-    return {"message": "Moving down!"}
+    bot.send_message(helper, "Не / Нехай gpt дасть більше інфи по питанню")
+    return {"message": "down"}
 
 
 @app.get("/right")
 async def move_right():
-    bot.send_message(helper, "Нахил вправо")
-    return {"message": "Moving right!"}
+    bot.send_message(helper, "Наступне речення")
+    return {"message": "right"}
 
 
 @app.get("/left")
 async def move_left():
-    bot.send_message(helper, "Нахил вліво")
-    return {"message": "Moving left!"}
+    bot.send_message(helper, "Прочитай речення з початку")
+    return {"message": "left"}
 
 
 @app.get("/tap")
 async def move_left():
-    bot.send_message(helper, "Тап по екрану")
-    return {"message": "Tap!"}
+    bot.send_message(helper, "Да / Почни читати відповідь з початку")
+    return {"message": "tap"}
 
 
 @app.get("/deploy")
 async def deploy():
     subprocess.run('sh deploy.sh', shell=True)
-    return {"message": "Deployed!"}
+    return {"message": "deployed"}
 
 
 if __name__ == "__main__":
