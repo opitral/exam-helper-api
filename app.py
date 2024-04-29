948cdf1ca8eaef81e499e2cef333d72a48e8bbc6
Володимир Слободян <opitral@proton.me>
2024-04-29 20:21:47 +0300

    add double handlers

--- app.py
+++ app.py
@@ -12,34 +12,64 @@ bot = TeleBot(getenv("TELEGRAM_BOT_TOKEN"))
 helper = getenv("TELEGRAM_HELPER_ID")
 
 
+@app.get("/tap")
+async def single_tap():
+    bot.send_message(helper, "Да")
+    return {"message": "single tap"}
+
+
+@app.get("/doubleTap")
+async def double_tap():
+    bot.send_message(helper, "Не")
+    return {"message": "tap"}
+
+
 @app.get("/up")
-async def move_up():
+async def single_up():
     bot.send_message(helper, "Наступне питання")
-    return {"message": ""}
+    return {"message": "single up"}
 
 
 @app.get("/down")
-async def move_down():
-    bot.send_message(helper, "Не / Нехай gpt дасть більше інфи по питанню")
-    return {"message": "down"}
+async def single_down():
+    bot.send_message(helper, "Попереднє питання")
+    return {"message": "single down"}
 
 
 @app.get("/right")
-async def move_right():
+async def single_right():
     bot.send_message(helper, "Наступне речення")
-    return {"message": "right"}
+    return {"message": "single right"}
 
 
 @app.get("/left")
-async def move_left():
+async def single_left():
     bot.send_message(helper, "Прочитай речення з початку")
-    return {"message": "left"}
+    return {"message": "single left"}
 
 
-@app.get("/tap")
-async def move_left():
-    bot.send_message(helper, "Да / Почни читати відповідь з початку")
-    return {"message": "tap"}
+@app.get("/doubleLeft")
+async def double_left():
+    bot.send_message(helper, "Прочитай повністю з початку")
+    return {"message": "double left"}
+
+
+@app.get("/doubleRight")
+async def double_right():
+    bot.send_message(helper, "медний бичок")
+    return {"message": "double right"}
+
+
+@app.get("/doubleDown")
+async def double_down():
+    bot.send_message(helper, "Дай більше інфи (gpt, google)")
+    return {"message": "double down"}
+
+
+@app.get("/doubleUp")
+async def double_up():
+    bot.send_message(helper, "1488")
+    return {"message": "double up"}
 
 
 @app.get("/deploy")
